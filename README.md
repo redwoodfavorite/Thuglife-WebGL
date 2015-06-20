bcd1050dc2432383263cf5bd7f0a8c343e7f54f2
redwoodfavorite <joseph.michael.sample@gmail.com>
2015-06-19 17:07:18 -0700

    feat: replacing renderer

--- README.md
+++ README.md
@@ -1,10 +1,1 @@
-# Thuglife-WebGL
-A lil' rapper for your WebGL.
-
-* Write your own damn shaders!
-* Bind your own damn buffers!
-* Set your own damn uniforms!
-
-A an extremely lightweight, low level, wrapper for WebGL.  Great for perf testing.
-
-Work in progress.
+A low-level WebGL wrapper good for perf testing and prototyping.
--- src/Renderer.js
+++ src/Renderer.js
@@ -1,4 +1,5 @@
-function WebGLRenderer(options) {
+
+function Renderer(options) {
 	this.canvas = document.createElement('canvas');
 	this.canvas.width = innerWidth;
 	this.canvas.height = innerHeight;
@@ -13,6 +14,12 @@ function WebGLRenderer(options) {
 
 	this.buffers = createBuffers.call(this, options.arrayBuffers, options.indexBuffers);
 
+	this.framebuffers = {};
+	this.renderbuffers = {};
+	this.textures = {};
+
+	if (options.framebuffers) this.initFrameBuffers(options.framebuffers);
+
 	this.boundArrayBuffer;
 	this.boundIndexBuffer;
 
@@ -21,19 +28,122 @@ function WebGLRenderer(options) {
 	this.updateSize(innerWidth, innerHeight);
 }
 
-WebGLRenderer.prototype.clear = function clear() {
+Renderer.prototype.clear = function clear() {
 	this.ctx.clear(this.ctx.COLOR_BUFFER_BIT | this.ctx.DEPTH_BUFFER_BIT);
 };
 
-WebGLRenderer.prototype.drawArrays = function drawArrays(drawType, stride) {
+Renderer.prototype.drawArrays = function drawArrays(drawType, stride) {
 	this.ctx.drawArrays(this.ctx[drawType], stride || 0, this.boundArrayBuffer.numItems);
 };
 
-WebGLRenderer.prototype.drawElements = function drawElements(drawType, offset) {
+Renderer.prototype.drawElements = function drawElements(drawType, offset) {
 	this.ctx.drawElements(this.ctx[drawType], this.boundIndexBuffer.numItems, this.ctx.UNSIGNED_SHORT, offset || 0);
 };
 
-WebGLRenderer.prototype.setAttribute = function setAttribute(attributeKey) {
+Renderer.prototype.disableAttribute = function disableAttribute(key) {
+    this.ctx.disableVertexAttribArray(this.attributeLocations[key]);
+};
+
+Renderer.prototype.enableAttribute = function enableAttribute(key) {
+    this.ctx.enableVertexAttribArray(this.attributeLocations[key]);
+};
+
+Renderer.prototype.createTexture = function createTexture(size, format, type, target, filters) {
+
+	// Create texture
+
+	var texture = this.ctx.createTexture();
+		texture.width = size[0];
+		texture.height = size[1];
+		texture.format = format ? this.ctx[format] : this.ctx['RGBA'];
+		texture.type = type ? this.ctx[type] : this.ctx['UNSIGNED_BYTE'];
+		texture.target = target ? this.ctx[target] : this.ctx['TEXTURE_2D'];
+
+
+	// Bind texture
+
+    this.ctx.bindTexture(texture.target, texture);
+
+    // Set texture options
+
+    this.ctx.texParameteri(texture.target, this.ctx.TEXTURE_MAG_FILTER, this.ctx[filters && filters.mag ? filters.mag : 'LINEAR']);
+    this.ctx.texParameteri(texture.target, this.ctx.TEXTURE_MIN_FILTER, this.ctx[filters && filters.min ? filters.min : 'LINEAR']);
+
+    if (!isPowerOfTwo(size[0], size[1])) {
+	    this.ctx.texParameteri(texture.target, this.ctx.TEXTURE_WRAP_S, this.ctx.CLAMP_TO_EDGE);
+	    this.ctx.texParameteri(texture.target, this.ctx.TEXTURE_WRAP_T, this.ctx.CLAMP_TO_EDGE);
+    }
+
+    // Set texture width and height
+
+    this.ctx.texImage2D(texture.target, 0, texture.format, texture.width, texture.height, 0, texture.format, texture.type, null);
+
+    // Unbind texture
+
+    this.ctx.bindTexture(texture.target, null);
+
+	return texture;
+}
+
+Renderer.prototype.initFrameBuffers = function initFrameBuffers(framebuffers) {
+	var accessor;
+	var size;
+
+	for (var i = 0; i < framebuffers.length; i++) {
+		accessor = framebuffers[i].name;
+		size = framebuffers[i].size;
+
+	    this.framebuffers[accessor] = this.ctx.createFramebuffer();
+	    this.renderbuffers[accessor] = this.ctx.createRenderbuffer();
+	    this.textures[accessor] = this.createTexture(size);
+
+    	this.bindFramebuffer(accessor);
+
+    	this.ctx.framebufferTexture2D(this.ctx.FRAMEBUFFER, this.ctx.COLOR_ATTACHMENT0, this.ctx.TEXTURE_2D, this.textures[accessor], 0);
+	    this.ctx.framebufferRenderbuffer(this.ctx.FRAMEBUFFER, this.ctx.DEPTH_ATTACHMENT, this.ctx.RENDERBUFFER, this.renderbuffers[accessor]);
+    	this.ctx.renderbufferStorage(this.ctx.RENDERBUFFER, this.ctx.DEPTH_COMPONENT16, size[0], size[1]);
+	}
+
+	this.bindFramebuffer(null);
+};
+
+Renderer.prototype.unbindTexture = function unbindTexture(key, active) {
+	var texture = this.textures[key];
+
+	if (active !== undefined) this.ctx.activeTexture(this.ctx.TEXTURE0 + active);
+
+	this.ctx.bindTexture(texture.target, null);	
+}
+
+Renderer.prototype.samplerToTextureSlot = function samplerToTextureSlot (uniformName, textureSlot) {
+	var uniformLocation = this.uniformLocations[uniformName];
+
+	if (uniformLocation === undefined) throw 'Sampler ' + uniformName + ' not found!';
+
+	return this.ctx.uniform1i(this.uniformLocations[uniformName], textureSlot);
+}
+
+Renderer.prototype.bindTexture = function bindTexture(key, active) {
+	var texture = this.textures[key];
+
+	if (active !== undefined) this.ctx.activeTexture(this.ctx.TEXTURE0 + active);
+
+	this.ctx.bindTexture(texture.target, texture);
+}
+
+Renderer.prototype.bindFramebuffer = function bindFramebuffer(key) {
+	var framebuffer = this.framebuffers[key];
+	var renderbuffer = this.renderbuffers[key];
+
+    this.ctx.bindFramebuffer(this.ctx.FRAMEBUFFER, framebuffer || null);
+    this.ctx.bindRenderbuffer(this.ctx.RENDERBUFFER, renderbuffer || null);
+};
+
+Renderer.prototype.viewport = function viewport(width, height, offsetX, offsetY) {
+	return this.ctx.viewport(offsetX || 0, offsetY || 0, width, height);
+}
+
+Renderer.prototype.setAttribute = function setAttribute(attributeKey) {
 	if (this.attributeLocations[attributeKey] === -1) throw 'ATTRIBUTE LOCATION NOT FOUND';
 	else if (!this.boundArrayBuffer) throw 'NO BOUND ARRAY_BUFFER';
 
@@ -45,7 +155,7 @@ WebGLRenderer.prototype.setAttribute = function setAttribute(attributeKey) {
 	);
 }
 
-WebGLRenderer.prototype.setBufferData = function setBufferData(key, data, size) {
+Renderer.prototype.setBufferData = function setBufferData(key, data, size) {
 	var buffer = this.bindBuffer(key);
 	var data = buffer.isIndex ? new Uint16Array(data) : new Float32Array(data);
 
@@ -55,7 +165,7 @@ WebGLRenderer.prototype.setBufferData = function setBufferData(key, data, size) 
 	buffer.numItems = data.length / size;
 }
 
-WebGLRenderer.prototype.bindBuffer = function bindBuffer(key) {
+Renderer.prototype.bindBuffer = function bindBuffer(key) {
 	var buffer = this.buffers[key];
 	this.ctx.bindBuffer(buffer.target, buffer);
 
@@ -65,15 +175,29 @@ WebGLRenderer.prototype.bindBuffer = function bindBuffer(key) {
 	return buffer;
 }
 
-WebGLRenderer.prototype.setUniform = function setUniform(uniformName, type, value) {
-	var location = this.uniformLocations[uniformName];
-
-	return this.ctx[type](location, false, value);
+Renderer.prototype.setDrawState = function setDrawState(funcName, value) {
+	return this.ctx[funcName](value);
 }
 
-WebGLRenderer.prototype.updateSize = function updateSize(width, height) {
-	//fix later
+Renderer.prototype.setUniform = function setUniform(uniformName, type, value) {
+	var location = this.uniformLocations[uniformName];
 
+    switch (type) {
+        case 'uniform4fv': return this.ctx.uniform4fv(location, value); break;
+        case 'uniform3fv': return this.ctx.uniform3fv(location, value); break;
+        case 'uniform2fv': return this.ctx.uniform2fv(location, value); break;
+        case 'uniform1fv': return this.ctx.uniform1fv(location, value); break;
+        case 'uniform1f' : return this.ctx.uniform1f(location, value);  break;
+        case 'uniformMatrix3fv': return this.ctx.uniformMatrix3fv(location, false, value); break;
+        case 'uniformMatrix4fv': return this.ctx.uniformMatrix4fv(location, false, value); break;
+    }
+	// if (type === 'uniform1f' || type === 'uniform1i') return this.ctx[type](location, value);
+
+	// return this.ctx[type](location, false, value);
+}
+
+Renderer.prototype.updateSize = function updateSize(width, height) {
+	//fix later
     this.ctx.viewport(0, 0, width, height);
 
 	this.canvas.width = width;
@@ -135,7 +259,7 @@ function createProgram(fSource, vSource) {
     ctx.linkProgram(shaderProgram);
 
     if (!ctx.getProgramParameter(shaderProgram, ctx.LINK_STATUS))
-    	throw ctx.LINK_STATUS;
+    	throw 'Link error: ' + this.ctx.getProgramInfoLog(shaderProgram);
 
     ctx.useProgram(shaderProgram);
 
@@ -167,3 +291,10 @@ function checkCompileStatus(shader) {
         console.error('1: ' + source.replace(/\n/g, function () { return '\n' + (i+=1) + ': '; }));
     }
 }
+
+function isPowerOfTwo(width, height) {
+    return (width & width - 1) === 0 
+        && (height & height - 1) === 0;
+};
+
+module.exports = Renderer;
